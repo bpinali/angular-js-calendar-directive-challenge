446818b813b809d9f5a0180028b65b45a72cdb7f
Ben Pinali <ben@benpinali.com>
2016-07-10 15:56:35 -0500

    Added angular directive, CSS rules, HTML calendar template.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,35 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
 
-// your controller and directive code go here
+.directive('calendar', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'calendar.html',
+        controller: function controller($scope, $element, $attrs) {
+
+            var date = new Date();
+            var currentMonth = date.getMonth();
+            var currentYear = date.getFullYear();
+
+            $scope.selectedMonth = currentMonth;
+            $scope.selectedYear = currentYear;
+
+            $scope.updateCalendar = function() {
+                currentMonth = $scope.selectedMonth;
+                $scope.createCalendar($scope.selectedYear, $scope.selectedMonth);
+            };
+
+            $scope.createCalendar = function(year, month) {
+                $scope.range = CalendarRange.getMonthlyRange(new Date(year, month));
+                $scope.range.days.forEach(addGray);
+            };
+
+            $scope.createCalendar(currentYear, currentMonth);
+
+            function addGray(element, index, array) {
+                if (element.month < currentMonth || element.month > currentMonth) {
+                    element.monthClass = "future-past-gray";
+                }
+            }
+        }
+    };
+});
